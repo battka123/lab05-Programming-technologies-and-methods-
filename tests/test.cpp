96a6c7e0f15eee0e19487cf804ac87be6f56974c
battka123 <i.suschin@yandex.ru>
2023-03-23 18:17:50 +0300

    Added files!

---
+++ tests/test.cpp
@@ -0,0 +1,55 @@
+#include <Account.h>
+#include <gtest/gtest.h>
+#include <Account.h>
+#include <Transaction.h>
+#include <gtest/gtest.h>
+
+TEST(Account, Banking){
+//создаём тестовый объект
+	Account test(0,0);
+//проверяем GetBalance (и заодно конструктор)
+	ASSERT_EQ(test.GetBalance(), 0);
+//проверяем, что фича lock работает нормально
+	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
+	test.Lock();
+	ASSERT_NO_THROW(test.ChangeBalance(100));
+//проверяем, что изменение баланса работает
+	ASSERT_EQ(test.GetBalance(), 100);
+//проверяем, что залочить уже залоченное нельзя
+	ASSERT_THROW(test.Lock(), std::runtime_error);
+//проверяем, что анлок работает
+	test.Unlock();
+	ASSERT_THROW(test.ChangeBalance(100), std::runtime_error);
+//радуемся жизни
+}
+
+TEST(Transaction, Banking){
+//создаём константы. base_fee для полноценного теста должен быть хотя бы 51.
+	const int base_A = 5000, base_B = 5000, base_fee = 100;
+//создаём тестовые объекты
+	Account Alice(0,base_A), Bob(1,base_B);
+	Transaction test_tran;
+//проверяем конструктор по умолчанию и сеттеры-геттеры
+	ASSERT_EQ(test_tran.fee(), 1);
+	test_tran.set_fee(base_fee);
+	ASSERT_EQ(test_tran.fee(), base_fee);
+//проверяем случаи когда транзакция не проходит
+	ASSERT_THROW(test_tran.Make(Alice, Alice, 1000), std::logic_error);
+	ASSERT_THROW(test_tran.Make(Alice, Bob, -50), std::invalid_argument);
+	ASSERT_THROW(test_tran.Make(Alice, Bob, 50), std::logic_error);
+	if (test_tran.fee()*2-1 >= 100)
+		ASSERT_EQ(test_tran.Make(Alice, Bob, test_tran.fee()*2-1), false);
+//проверяем, что всё правильно лочится
+	Alice.Lock();
+	ASSERT_THROW(test_tran.Make(Alice, Bob, 1000), std::runtime_error);
+	Alice.Unlock();
+//проверяем что если входные параметры правильные, то транзакция проходит православно
+	ASSERT_EQ(test_tran.Make(Alice, Bob, 1000), true);
+	ASSERT_EQ(Bob.GetBalance(), base_B+1000);	
+	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
+//проверяем что транзакция не проходит, если не хватает средств
+	ASSERT_EQ(test_tran.Make(Alice, Bob, 3900), false);
+	ASSERT_EQ(Bob.GetBalance(), base_B+1000);	
+	ASSERT_EQ(Alice.GetBalance(), base_A-1000-base_fee);
+}
+s
